a4632224a68268d5ae662a22990a73bb40d913ad
AFKLenb <markermma@gmail.com>
2024-01-27 13:53:47 +0300

    Сделали компоненты и стили

---
+++ frontend/app/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import Styles from "@/app/components/Button/Button.module.sass"
+import Link from "next/link"
+export default function Button(props) {
+    if (props.href){
+        return(
+            <Link href={props.href} className={Styles.button} >
+                {props.children}
+            </Link>
+        )
+    }
+    return (
+    <button className={Styles.button}>
+            {props.children}
+    </button>
+  )
+}
---
+++ frontend/app/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styles from '@/app/components/Header/Header.module.sass';
+
+export default function Header() {
+  return (
+    <header className={`${Styles.header} mt-10`}>
+        <div className={Styles.header__container}>
+            hello
+        </div>
+    </header>
+  )
+}
+
